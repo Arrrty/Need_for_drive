fee0190d5dd9d5d9f68cfd30001f28e57993aeed
Artyom Yakimov <ykim-49464@mail.ru>
2020-06-05 12:26:34 +0300

    Updates

---
+++ swipconfig.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+};
